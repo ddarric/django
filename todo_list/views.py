197078c049104a57eeac753f0a57127b72a5c1cb
Didier Darricau <ddarrica@bouyguestelecom.fr>
2019-11-04 16:15:38 +0100

    End of day push

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo_list.models import Task
 from todo_list.forms import NewTaskForm
 from datetime import date
@@ -10,35 +10,33 @@ def main_page(request):
         print(request.POST)
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            print(type(form.cleaned_data['end_date']))
             task = Task(
                 title = form.cleaned_data['title'],
                 description = form.cleaned_data['description'],
                 end_date = form.cleaned_data['end_date']
             )
             task.save()
-            print('abc')
-            #Perform database insertions
-            """
-            'tasks': tasks,
-            'form': form,
-            'title': form.cleaned_data['title'],
-            'description': form.cleaned_data['description'],
-            #'end_date': form.cleaned_data['end_date'] 
-            """
-            #Return error or ok messages
-        else:
-            print('toto')
     else:
         form = NewTaskForm()
     tasks = Task.objects.all()
     return render(request, 'main_page.html', {'form': form, 'tasks': tasks})
 
-    """
-    tasks = Task.objects.all()
-    context = {
-        'tasks': tasks
-    }
-    form = NewTaskForm()
-    return render(request, 'main_page.html', context)
-    """
+def task_details(request, pk):
+    task = Task.objects.filter(pk = pk)
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            #if title/ description/ end_date
+            task.update(title = form.cleaned_data['title'])
+            task.update(description = form.cleaned_data['description'])
+            task.update(end_date = form.cleaned_data['end_date'])
+            return redirect('main_page')
+    else:
+        form = NewTaskForm()
+    return render(request, 'task_details.html', {'form': form, 'task': task[0]})
+
+def delete_task(request, pk):
+    #if request.method == 'POST':
+    #print(type(Task.objects.filter(pk = pk)[0]))
+    Task.objects.filter(pk = pk).delete()
+    return redirect('main_page')
